33e553edeb1e49bcaa91ebe6f774b7a1d9e2cba9
Gongqi Huang <gongqih@hotmail.com>
2022-08-04 11:44:09 -0700

    fix minor

--- src/exception.rs
+++ src/exception.rs
@@ -1,9 +1,10 @@
 use crate::{UART, timer, system_off, gic};
+use crate::gic::GIC;
 use core::fmt::Write;
 use core::arch::asm;
 
-#[repr(u16)]
 #[derive(Debug)]
+#[repr(u16)]
 pub enum Description {
     CurrentElSP0,
     CurrentElSPx,
@@ -11,8 +12,8 @@ pub enum Description {
     LowerElAArch32,
 }
 
-#[repr(u16)]
 #[derive(Debug)]
+#[repr(u16)]
 pub enum Kind {
     Synchronous,
     IRQ,
@@ -20,15 +21,15 @@ pub enum Kind {
     SError,
 }
 
-#[repr(C)]
 #[derive(Debug)]
+#[repr(C)]
 pub struct Info {
     kind: Kind,
     desc: Description,
 }
 
-#[repr(C)]
 #[derive(Debug)]
+#[repr(C)]
 pub struct Frame {
     pstate: u64,        // spsr_el1
     address: u64,        // elr_el1 return address
@@ -37,33 +38,60 @@ pub struct Frame {
     link_register: u64, // x30
 }
 
+#[derive(Debug)]
+#[repr(u32)]
+enum InterruptIndex {
+    Timer = timer::EL1_PHYSICAL_TIMER,
+}
+
+// impl InterruptIndex {
+    // fn as_u32(self) -> u32 {
+        // self as u32
+    // }
+// }
+
+const INTERRUPTS: &[(u32, &dyn Fn(u32, &Frame))] = &[
+    (InterruptIndex::Timer as u32, &timer_interrupt_handler),
+];
+
 #[no_mangle]
 pub extern "C" fn exception_handler(info: Info, frame: &Frame) {
-
-    match info.kind {
-        Kind::Synchronous => todo!(),
-        Kind::IRQ => interrupt_handler(frame),
-        Kind::FIQ => todo!(),
-        Kind::SError => todo!(),
+    match info.desc {
+        _ => match info.kind {
+            Kind::Synchronous => todo!(),
+            Kind::IRQ => {
+                for &(irq, handler) in INTERRUPTS.iter() {
+                    if gic::is_pending(irq) {
+                        handler(irq, frame);
+                        // interrupt_handler(irq, frame);
+                    }
+                }
+            }
+            Kind::FIQ => todo!(),
+            Kind::SError => todo!(),
+        }
     }
+
     // UART.map(|u| write!(u, "{:?}\n{:#?}\n", info, frame));
     // unsafe {
         // system_off();
     // }
 }
 
-use alloc::vec::Vec;
-fn interrupt_handler(frame: &Frame) {
-    let a = (0..100).filter(|&irq| gic::is_pending(irq)).collect::<Vec<u32>>();
-    for &irq in a.iter() {
-        if gic::is_pending(irq) {
-            UART.map(|u| write!(u, "{:?}\n", a));
-            UART.map(|u| write!(u, "{:?}\n", irq));
-            gic::clear(30);
-            timer::reset_tval();
-        }
-    }
+fn timer_interrupt_handler(irq: u32, frame: &Frame) {
+    // UART.map(|u| write!(u, "{:?}\n", irq));
+    UART.map(|u| write!(u, "."));
+    timer::reset_tval();
+    gic::clear(irq);
 }
+
+// fn interrupt_handler(irq: u32, frame: &Frame) {
+    // match irq {
+        // timer::EL1_PHYSICAL_TIMER => {
+        // }
+        // _ => unimplemented!("failed to handle irq {}", irq),
+    // }
+// }
 
 pub fn load_table() {
     unsafe {
