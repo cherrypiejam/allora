34edc598ce7490f77e6869ee06df6a7a0575b234
Gongqi Huang <gongqih@hotmail.com>
2023-09-11 17:15:55 -0400

    nits

--- run_nonet.sh
+++ run_nonet.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 qemu-system-aarch64 -M virt,virtualization=on -cpu cortex-a53 -smp cpus=4 -m 1024M -display none -serial stdio -global virtio-mmio.force-legacy=false -device virtio-rng-device -drive if=none,cache=directsync,file=test.img,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -kernel $1
-# qemu-system-aarch64 -M virt -cpu cortex-a53 -smp cpus=4 -m 1024M -display none -serial stdio -global virtio-mmio.force-legacy=false -device virtio-rng-device -drive if=none,cache=directsync,file=test.img,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -kernel $1
